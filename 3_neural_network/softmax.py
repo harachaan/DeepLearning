030620c1d208a793937a33a12bf75836999b4d2e
harachaan <hr1021ry@icloud.com>
2022-09-16 11:56:41 +0900

    add source code

--- 3_neural_network/softmax.py
+++ 3_neural_network/softmax.py
@@ -1,10 +1,13 @@
 # ソフトマックス関数のpythonの関数としての定義
 
+import numpy as np
+
+
 def softmax(a):
-    c = np.max(a) # オーバーフローを考慮
+
+    c = np.max(a)  # オーバーフローを考慮
     exp_a = np.exp(a-c)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
 
     return y
-
